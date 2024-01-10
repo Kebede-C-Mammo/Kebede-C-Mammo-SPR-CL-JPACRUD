9ed53cc3a7878617e729ccbcb6e1b282df5b1eb5
Kebede Mammo <mkebede2020@gmail.com>
2024-01-10 02:38:52 +0000

    Completed Spring JPA CRUD lab.

--- src/main/java/Lab/Repository/StoreRepository.java
+++ src/main/java/Lab/Repository/StoreRepository.java
@@ -4,4 +4,8 @@ import Lab.Model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    public int deleteStore(long id);
+    
 }
+    
